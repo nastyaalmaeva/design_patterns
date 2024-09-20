fae0a48d77fa79cf1fbc3bfa67d80a019de5acce
nastyaalmaeva <nastyaalmaeva@mail.ru>
2024-09-20 18:49:08 +0300

    Task 4.3 (The product of the divisors of a number whose sum of digits is less than the sum of digits of the original number) (LW1)

--- introduction_to_ruby/working_with_numbers.rb
+++ introduction_to_ruby/working_with_numbers.rb
@@ -36,6 +36,29 @@ def count_odd_digits_above_three(number)
 	return odd_digits_count
 end
 
+def digit_sum(number)
+	sum_of_digits = 0
+	while number > 0 do
+		digit = number % 10
+		sum_of_digits += digit
+		number /= 10
+	end
+	return sum_of_digits
+end
+
+def product_of_divisors_with_digit_sum_less_than(number)
+	if number <= 1 then
+		return 0
+	end
+	product_of_divisors = 1
+	for divisor in 1..number do
+		if number % divisor == 0 && digit_sum(divisor) < digit_sum(number) then
+			product_of_divisors *= divisor
+		end
+	end
+	return product_of_divisors
+end
+
 begin
 	puts "\n"
 	print "Enter a number: "
@@ -48,6 +71,7 @@ begin
 		puts "\n"
 		puts "The sum of the prime divisors of the number #{integer_part} is #{prime_divisor_sum(integer_part)}."
 		puts "The number of odd digits in #{integer_part} that are greater than 3 is #{count_odd_digits_above_three(integer_part)}."
+		puts "The product of the divisors of the number #{integer_part} whose digit sum is less than the sum of the digits of the original number is #{product_of_divisors_with_digit_sum_less_than(integer_part)}."
 	else
 		puts "\nThe input is not a valid positive integer."
 	end
