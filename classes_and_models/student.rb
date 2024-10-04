0f76d3c5f5f560f566413c81e48ff94b3aa13451
nastyaalmaeva <nastyaalmaeva@mail.ru>
2024-10-04 10:25:26 +0300

    Task 1.3 (Eliminate duplication in Student class constructor and accessors) (LW2)

--- classes_and_models/student.rb
+++ classes_and_models/student.rb
@@ -1,4 +1,7 @@
 class Student
+	attr_reader :student_id, :surname, :name, :patronymic, :phone_number, :telegram, :email_address, :git
+	attr_writer :student_id, :surname, :name, :patronymic, :phone_number, :telegram, :email_address, :git
+
 	def initialize(surname, name, patronymic, student_id=nil, phone_number=nil, telegram=nil, email_address=nil, git=nil)
 		self.student_id = student_id
 		self.surname = surname
@@ -8,70 +11,6 @@ class Student
 		self.telegram = telegram
 		self.email_address = email_address
 		self.git = git
-	end
-	
-	def student_id
-		@student_id
-	end
-	
-	def student_id=(value)
-		@student_id = value
-	end
-	
-	def surname
-		@surname
-	end
-	
-	def surname=(value)
-		@surname = value
-	end
-	
-	def name
-		@name
-	end
-	
-	def name=(value)
-		@name = value
-	end
-	
-	def patronymic
-		@patronymic
-	end
-	
-	def patronymic=(value)
-		@patronymic = value
-	end
-	
-	def phone_number
-		@phone_number
-	end
-	
-	def phone_number=(value)
-		@phone_number = value
-	end
-	
-	def telegram
-		@telegram
-	end
-	
-	def telegram=(value)
-		@telegram = value
-	end
-	
-	def email_address
-		@email_address
-	end
-	
-	def email_address=(value)
-		@email_address = value
-	end
-	
-	def git
-		@git
-	end
-	
-	def git=(value)
-		@git = value
 	end
 	
 	def print_info
