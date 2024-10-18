bb28456daa5f24082fe5a7ea9750bf791ce39de3
nastyaalmaeva <nastyaalmaeva@mail.ru>
2024-10-19 01:09:06 +0300

    Task 2.3 (Improved logic of the get_info method) (LW2)

--- classes_and_models/main.rb
+++ classes_and_models/main.rb
@@ -2,59 +2,66 @@ require_relative 'student.rb'
 
 begin
 	first_student = Student.new(
-					surname: "Almaeva",
-					name: "Anastasia",
-					patronymic: "Ilyinichna",
-					student_id: 1,
-					phone_number: "+7(905)-123-45-67",
-					telegram: "@nastya",
-					email_address: "nastya@gmail.com",
-					git: "https://github.com/nastyaalmaeva")
+		surname: "Almaeva",
+		name: "Anastasia",
+		patronymic: "Ilyinichna",
+		student_id: 1,
+		phone_number: "+7(905)-123-45-67",
+		telegram: "@nastya",
+		email_address: "nastya@gmail.com",
+		git: "https://github.com/nastyaalmaeva"
+	)
 	second_student = Student.new(
-					surname: "Kuznetsov",
-					name: "Dmitry",
-					patronymic: "Nikolaevich",
-					student_id: 2,
-					telegram: "@dmitry_k",
-					email_address: "dmitry.kuznetsov@gmail.com",
-					git: "https://github.com/dmitry_kuznetsov")
+		surname: "Kuznetsov",
+		name: "Dmitry",
+		patronymic: "Nikolaevich",
+		student_id: 2,
+		telegram: "@dmitry_k",
+		email_address: "dmitry.kuznetsov@gmail.com",
+		git: "https://github.com/dmitry_kuznetsov"
+	)
 	third_student = Student.new(
-					surname: "Fedorov",
-					name: "Maxim",
-					patronymic: "Olegovich",
-					student_id: 3,
-					phone_number: "+7(925)-888-99-11",
-					telegram: "@max_fed",
-					git: "https://github.com/max_fedorov")
+		surname: "Fedorov",
+		name: "Maxim",
+		patronymic: "Olegovich",
+		student_id: 3,
+		phone_number: "+7(925)-888-99-11",
+		telegram: "@max_fed",
+		git: "https://github.com/max_fedorov"
+	)
 	fourth_student = Student.new(
-					surname: "Ivanov",
-					name: "Sergey",
-					patronymic: "Viktorovich",
-					student_id: 4,
-					phone_number: "+7(910)-111-22-33")
+		surname: "Ivanov",
+		name: "Sergey",
+		patronymic: "Viktorovich",
+		student_id: 4,
+		phone_number: "+7(910)-111-22-33"
+	)
 	fifth_student = Student.new(
-					surname: "Petrov",
-					name: "Alexey",
-					patronymic: "Dmitrievich",
-					student_id: 5)
-	
+		surname: "Petrov",
+		name: "Alexey",
+		patronymic: "Dmitrievich",
+		student_id: 5
+	)
+
 	puts first_student
 	puts second_student
 	puts third_student
 	puts fourth_student
 	puts fifth_student
-	
+
 	first_student.set_contacts(phone_number: "+7(888)-777-66-55")
 	second_student.set_contacts(email_address: "dmitry_dev@gmail.com")
 	third_student.set_contacts(telegram: "@maxim_develop")
-	
-	puts "\n\nAfter changing contacts (get_info):"
+
+	puts "\n"
+
+	puts "After changing contacts (using get_info):"
 	puts first_student.get_info
 	puts second_student.get_info
 	puts third_student.get_info
 	puts fourth_student.get_info
 	puts fifth_student.get_info
-	
+
 rescue ArgumentError => e
-  print "An error occurred: #{e.message}"
+	print "An error occurred: #{e.message}"
 end
--- classes_and_models/student.rb
+++ classes_and_models/student.rb
@@ -1,7 +1,7 @@
 class Student
 	attr_accessor :student_id
 	attr_reader :surname, :name, :patronymic, :phone_number, :telegram, :email_address, :git
-	
+
 	def initialize(surname:, name:, patronymic:, student_id: nil, phone_number: nil, telegram: nil, email_address: nil, git: nil)
 		self.student_id = student_id
 		self.surname = surname
@@ -10,27 +10,27 @@ class Student
 		self.git = git
 		self.set_contacts(phone_number: phone_number, telegram: telegram, email_address: email_address)
 	end
-	
+
 	def set_contacts(phone_number: nil, telegram: nil, email_address: nil)
 		if self.class.valid_format_phone_number?(phone_number)
 			@phone_number = phone_number
 		else
 			raise ArgumentError, "Wrong phone number format. Expected format: +7(XXX)-XXX-XX-XX, where X is a digit."
 		end
-		
+
 		if self.class.valid_format_telegram?(telegram)
 			@telegram = telegram
 		else
 			raise ArgumentError, "Wrong telegram format. Expected format: @ followed by 5 or more English alphanumeric characters or underscores."
 		end
-		
+
 		if self.class.valid_format_email_address?(email_address)
 			@email_address = email_address
 		else
 			raise ArgumentError, "Wrong email address format. Expected format: name@domain.com."
 		end
 	end
-	
+
 	def surname=(surname)
 		if self.class.valid_format_name?(surname)
 			@surname = surname
@@ -38,7 +38,7 @@ class Student
 			raise ArgumentError, "Wrong surname format. Expected format: English letters (A-Z) or Cyrillic letters (А-Я), optionally hyphenated."
 		end
 	end
-	
+
 	def name=(name)
 		if self.class.valid_format_name?(name)
 			@name = name
@@ -46,7 +46,7 @@ class Student
 			raise ArgumentError, "Wrong name format. Expected format: English letters (A-Z) or Cyrillic letters (А-Я), optionally hyphenated."
 		end
 	end
-	
+
 	def patronymic=(patronymic)
 		if self.class.valid_format_name?(patronymic)
 			@patronymic = patronymic
@@ -54,7 +54,7 @@ class Student
 			raise ArgumentError, "Wrong patronymic format. Expected format: English letters (A-Z) or Cyrillic letters (А-Я), optionally hyphenated."
 		end
 	end
-	
+
 	def git=(git)
 		if self.class.valid_format_git?(git)
 			@git = git
@@ -62,47 +62,45 @@ class Student
 			raise ArgumentError, "Wrong git format. Expected format: http://github.com/username or https://github.com/username."
 		end
 	end
-	
+
 	def git_present?
 		!@git.nil?
 	end
-	
+
 	def contact_present?
 		!@phone_number.nil? || !@telegram.nil? || !@email_address.nil?
 	end
-	
+
 	def validate?
 		self.git_present? && self.contact_present?
 	end
-	
+
 	def get_surname_with_initials
 		"#{surname} #{name[0].upcase}. #{patronymic[0].upcase}."
 	end
 
 	def get_git
-		git ? git : "no data"
+		git
 	end
 
 	def get_contact_info
 		if phone_number
-			return "#{phone_number} (Type: phone number)"
+			return "#{phone_number} [phone number]"
 		elsif telegram
-			return "#{telegram} (Type: telegram)"
+			return "#{telegram} [telegram]"
 		elsif email_address
-			return "#{email_address} (Type: email address)"
+			return "#{email_address} [email address]"
 		else
-			return "no data"
+			return nil
 		end
 	end
 
 	def get_info
-		"╔═══════════════════════════════════════════════════╗\n" \
-		" Surname and initials: #{get_surname_with_initials}\n" \
-		" Git: #{get_git}\n" \
-		" Contact: #{get_contact_info}\n" \
-		"╚═══════════════════════════════════════════════════╝"
+		"Surname and initials: #{get_surname_with_initials ? get_surname_with_initials : 'No data'} | " \
+		"Git: #{get_git ? get_git : 'No data'} | " \
+		"Contact: #{get_contact_info ? get_contact_info : 'No data'}"
 	end
-	
+
 	def to_s
 		"╔═══════════════════════════════════════════════════╗\n" \
 		" Student ID: #{@student_id ? @student_id : "No data"}\n" \
@@ -116,7 +114,7 @@ class Student
 		" Validation Status: #{self.validate? ? "Passed" : "Failed"}\n" \
 		"╚═══════════════════════════════════════════════════╝"
 	end
-	
+
 	def self.valid_format_name?(name)
 		if name.match(/^[A-Za-z]+(?:-[A-Za-z]+)?$/) || name.match(/^[А-ЯЁа-яё]+(?:-[А-ЯЁа-яё]+)?$/)
 			return true
@@ -124,7 +122,7 @@ class Student
 			return false
 		end
 	end
-	
+
 	def self.valid_format_phone_number?(phone_number)
 		if phone_number.nil? || phone_number.match(/^\+7\(\d{3}\)-\d{3}-\d{2}-\d{2}$/)
 			return true
@@ -132,7 +130,7 @@ class Student
 			return false
 		end
 	end
-	
+
 	def self.valid_format_telegram?(telegram)
 		if telegram.nil? || telegram.match(/@[a-zA-Z0-9_]{5,}$/)
 			return true
@@ -140,7 +138,7 @@ class Student
 			return false
 		end
 	end
-	
+
 	def self.valid_format_email_address?(email_address)
 		if email_address.nil? || email_address.match(/^[\w+_.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
 			return true
@@ -148,7 +146,7 @@ class Student
 			return false
 		end
 	end
-	
+
 	def self.valid_format_git?(git)
 		if git.nil? || git.match(/^https?:\/\/(www\.)?github\.com\/[a-zA-Z0-9_-]+\/?$/)
 			return true
