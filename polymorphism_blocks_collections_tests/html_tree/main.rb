1e60fbdb7b4c98a1e05c8361515328d883ab6514
nastyaalmaeva <nastyaalmaeva@mail.ru>
2024-12-14 02:40:14 +0300

    Task 3.8 (Updated main.rb and example.html to integrate new features) (LW3)

--- polymorphism_blocks_collections_tests/html_tree/main.rb
+++ polymorphism_blocks_collections_tests/html_tree/main.rb
@@ -17,22 +17,57 @@ def read_html_file_to_string(file_path)
 	end
 end
 
+def print_opening_tags_bfs(tree)
+    iterator = tree.bfs_iterator
+    iterator.each { |node| puts node.opening_tag }
+end
+
+def print_opening_tags_dfs(tree)
+    iterator = tree.dfs_iterator
+    iterator.each { |node| puts node.opening_tag }
+end
+
+def print_elements_with_attributes_bfs(tree)
+    iterator = tree.bfs_iterator
+    elements_with_attributes = iterator.select { |node| node.attributes && !node.attributes.empty? }
+    elements_with_attributes.each { |node| puts node }
+end
+
+def print_elements_with_attributes_dfs(tree)
+    iterator = tree.dfs_iterator
+    elements_with_attributes = iterator.select { |node| node.attributes && !node.attributes.empty? }
+    elements_with_attributes.each { |node| puts node }
+end
+
+
 begin
 	html_content = read_html_file_to_string("example.html")
 	puts "HTML Content of the file:"
-	puts "\n"
 	puts html_content
+
 	tree = Tree.new(html_content)
+
+	puts "\n"
 	puts "\n"
 
-	div = Tag.new(
-		name: "div",
-		attributes: { class: "container" },
-		content: "This is a simple example of using the Tag class."
-	)
-	
-	puts "HTML Tag:"
-	puts div
+	puts "BFS:"
+	print_opening_tags_bfs(tree)
+
+	puts
+
+	puts "DFS:"
+	print_opening_tags_dfs(tree)
+
+	puts "\n"
+	puts "\n"
+
+	puts "Tags with attributes (BFS):"
+	print_elements_with_attributes_bfs(tree)
+
+	puts
+
+	puts "Tags with attributes (DFS):"
+	print_elements_with_attributes_dfs(tree)
 
 rescue => exception
 	puts "Error: #{exception.message}"
